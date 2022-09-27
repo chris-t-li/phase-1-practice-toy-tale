90bd16d517a2a20d89891e52bf12c1d2c7f18130
Chris Li <chris.lty07@gmail.com>
2022-09-26 22:11:34 -0400

    Completed Toy Tale Assignment

--- src/index.js
+++ src/index.js
@@ -12,4 +12,67 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys/')
+    .then(res => res.json())
+    .then(data => {
+      for (const id in data) {
+        renderCards(data[id])
+      }
+    })
+
+  function renderCards(data) {
+    const card = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const button = document.createElement('button');
+
+    card.className = 'card';
+    h2.textContent = data.name;
+    img.src = data.image;
+    img.className = "toy-avatar";
+    p.textContent = `${data.likes} likes`;
+    button.textContent = "like";
+    document.querySelector('#toy-collection').appendChild(card);
+    card.append(h2, img, p, button)
+
+    button.addEventListener('click', ()=> {
+      fetch(`http://localhost:3000/toys/${data.id}`,{
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          "likes": data.likes += 1
+        })
+      })
+      p.textContent = `${data.likes} likes`;
+    })
+  }
+
+  document.querySelector('.submit').addEventListener('click', e => {
+    e.preventDefault();
+    const newToyName = document.querySelector('#newName')
+    const newImage = document.querySelector('#newImage')
+    const newToyObj = {
+      name: newToyName.value,
+      image: newImage.value,
+      likes: 0
+    }
+
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(newToyObj)
+    }
+
+    fetch('http://localhost:3000/toys/', configObj)
+      .then(res => res.json())
+      .then(data => renderCards(data))
+  })
 });
